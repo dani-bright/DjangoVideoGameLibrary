f859d3ba0257331f57472b6d9ea143a5cb6a0307
dbebli <beblidaniel@gmail.com>
2020-06-01 12:44:32 +0200

    create models

--- videogame/website/models.py
+++ videogame/website/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField(default=timezone.now, verbose_name="Date de parution")
+    content = models.TextField(null=True)
+
+    class Meta:
+        verbose_name = "article"
+        ordering = ['date']
+
+    def __str__(self):
+        return self.title
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    editor = models.CharField(max_length=42)
+    img = models.TextField(null=True)
+    description =models.TextField(null=False)
+    releaseDate = models.DateTimeField(verbose_name="Date de parution")
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['releaseDate']
+
+    def __str__(self):
+        return self.title
+
+class Favorite(models.Model):
+    user = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
