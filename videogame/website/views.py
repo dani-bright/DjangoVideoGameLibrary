165f8f127d16e0a9e80c3067a445265de0436d0b
Winda <azis.widad@gmail.com>
2020-06-01 11:56:02 +0200

    front register login

--- videogame/website/views.py
+++ videogame/website/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'website/home.html', locals())
+
+def register(request):
+    return render(request, 'website/register.html', locals())
+
+def login(request):
+    return render(request, 'website/login.html', locals())
