f071c4a5cdba1d6913e598c90aacb54a9e40cdf9
dbebli <beblidaniel@gmail.com>
2020-06-01 13:09:19 +0200

    corrections

--- videogame/website/models.py
+++ videogame/website/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True, blank=True, upload_to="avatars/")
 
     def __str__(self):
         return self.user.username
@@ -17,6 +19,8 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField(default=timezone.now, verbose_name="Date de parution")
     content = models.TextField(null=True)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+
 
     class Meta:
         verbose_name = "article"
